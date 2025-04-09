0d625a045e5c886e91a3174ea55874b0d85b23d3
tfreed804 <tfreed804@gmail.com>
2025-04-09 11:46:51 -0400

    4/9/25 -- 11:45AM: imported constants from constants.py, initialized pygame within the main() function, created infinite game loop, updated comments

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
 # this allows us to use code from the open-source library throughout this file
 import pygame
 
+#import the variables from constants.py
+from constants import *
+
 def main():
+    #initialize pygame and set screen dimensions for the game
+    pygame.init
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    #create an infinite "game" loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        screen.fill((0, 0, 0))
+        pygame.display.flip()
+
+    #print information to the user regarding game initialization process
     print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
