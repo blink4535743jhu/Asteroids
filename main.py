6b60105f095d2b73b251ad5294520fcf43a847d1
tfreed804 <tfreed804@gmail.com>
2025-04-24 17:27:27 -0400

    4/24/25 -- 5:26PM: imported the AsteroidField class from boot.dev, completed the update method of asteroid.py, created an asteroids group in main.py, updated comments

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # this allows us to use code from the open-source library throughout this file
 import pygame
 
-#import the variables from constants.py and player class from player.py
+#import the variables from constants.py and player / asteroid classes
 from constants import *
 from player import *
+from asteroid import *
+from asteroidfield import *
 
 def main():
     #initialize pygame and set screen dimensions for the game
@@ -17,11 +19,22 @@ def main():
     #create 2 groups named updatable and drawable
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    #set both groups as containers for the Player
+    asteroids = pygame.sprite.Group()
+
+    #set both groups as containers for the Player class
     Player.containers = (updatable, drawable)
+
+    #set all groups as containers for the Asteroid class
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    #set the updatable group as the container for the AsteroidField Class
+    AsteroidField.containers = (updatable)
 
     #create an object of the player class
     player = Player(x, y)
+
+    #create an object of the AsteroidField Class
+    asteroid = AsteroidField()
 
     
 
